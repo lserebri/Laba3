7eea23d550beda51f000b1acfc9db6eb30aaf9d3
Леонид Серебряков <lserebryakov.0@gmail.com>
2019-04-07 12:12:48 +0300

    laba-3

--- OP3/Program.cs
+++ OP3/Program.cs
@@ -53,50 +53,10 @@ namespace OP3
             {
                 Console.WriteLine(Numbers.Dequeue());
             }
-            /*while(Operators.Count > 0)
-            {
-                if(args[i] == "(" || args[i] == ")" || args[i] == "*" 
-                    || args[i] =="/" || args[i] == "+" || args[i] == "-")
-                {
-                    if (i == 0)
-                        Operators.Push(args[i]);
-                    else
-                    {
-                        if ((Operators.Peek() == "*" || Operators.Peek() == "/") && (args[i] == "+" || args[i] == "-"))
-                        {
-                            Numbers.Enqueue(Operators.Pop());
-                            Operators.Push(args[i]);
-                        }
-                        else
-                        {
-                            Operators.Push(args[i]);
-                            Console.WriteLine("a");
-                        }
-                    }
-
-                }
-                else
-                {
-                    Numbers.Enqueue(args[i]);
-                }
-                i++;
-            }
-
-            while (Operators.Count > 0)
-            {
-                Numbers.Enqueue(Operators.Pop());
-            }
-            while (Numbers.Count > 0)
-            {
-                Console.WriteLine(Numbers.Dequeue());
-            }*/
-
             return (Numbers);
         }
         static void Main(string[] args)
         {
-           
-            string[] array = {"10", "*", "9", "+", "5"};
             Stack<string> Operators = new Stack<string>(args.Length);
             Queue<string> Numbers = new Queue<string>(args.Length);
             ToPostFix(args, Operators, Numbers);
