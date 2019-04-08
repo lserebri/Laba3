9ac579a0df2c192149fd46eddb71bb1b75846189
Леонид Серебряков <lserebryakov.0@gmail.com>
2019-04-08 23:08:32 +0300

    Поправил hash

--- OP3/Program.cs
+++ OP3/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Stack;
 using Queue;
 using System;
-//using System.Collections.Generic;
+using System.Collections;
 
 
 namespace OP3
@@ -13,53 +13,155 @@ namespace OP3
         {
             int res;
             bool isInt = Int32.TryParse(args, out res);
-            Console.WriteLine(isInt);
             return isInt;
+        }
+        static void Output(Stack<double> Output)
+        {
+            while(Output.Count > 0)
+            {
+                Console.WriteLine(Output.Pop());
+            }
+        }
+        static void HashTable(Queue<string> List)
+        {
+            Stack<double> Numbers = new Stack<double>(5);
+            while(List.Count > 0)
+            {
+                string s = List.Dequeue();
+                if (IsInt(s))
+                {
+                    Numbers.Push(Convert.ToDouble(s));
+                }
+                else
+                {
+                    if (Numbers.Count >= 2)
+                    {
+                        double value1, value2;
+                        value1 = Numbers.Pop();
+                        value2 = Numbers.Pop();
+                        double result = 0.0;
+                        if (s == "+")
+                        {
+                            result = addValues(value1, value1);
+                        }
+                        else if (s == "-")
+                        {
+                            result = subtractValues(value1, value1);
+                        }
+                        else if (s == "*")
+                        {
+                            result = multiplyValues(value1, value2);
+                        }
+                        else if (s == "/")
+                        {
+                            result = divideValues(value1, value1);
+                        }
+
+                        Numbers.Push(result);
+                    }
+                }
+            }
+            Output(Numbers);
         }
         static Queue<string> ToPostFix(string[] args, Stack<string> Operators, Queue<string> Numbers)
         {
             for(int i = 0; i < args.Length; i++)
             {
-                if(IsInt(args[i]) == true)
+                if(IsInt(args[i]))
                 {
                     Numbers.Enqueue(args[i]);
                 }
-                else
+                else if (Operators.Peek() == "^")
                 {
-                    if (Operators.IsEmpty() == true)
+                    Operators.Push(args[i]);
+                }
+                else if (operatorPrecedence(args[i]) != -1)
+                { 
+                    while(Operators.Count != 0 && operatorPrecedence(Operators.Peek()) >= operatorPrecedence(args[i]))
                     {
-                        Operators.Push(args[i]);
+                        Numbers.Enqueue(Operators.Pop());
                     }
-                    else
+                    Operators.Push(args[i]);
+                }    
+                else if(args[i] == "(")
+                {
+                     Operators.Push(args[i]);
+                }
+                else if(args[i] == ")")
+                {
+                    while(Operators.Peek() != "(")
                     {
-                        if ((Operators.Peek() == "*" || Operators.Peek() == "/") && (args[i] == "+" || args[i] == "-"))
-                        {
-                            Numbers.Enqueue(Operators.Pop());
-                            Operators.Push(args[i]);
-                        }
-                        else
-                        {
-                            Operators.Push(args[i]);
-                            Console.WriteLine("a");
-                        }
+                        Numbers.Enqueue(Operators.Pop());
                     }
+                    Operators.Pop();
                 }
             }
             while (Operators.Count > 0)
             {
                 Numbers.Enqueue(Operators.Pop());
             }
-            while (Numbers.Count > 0)
-            {
-                Console.WriteLine(Numbers.Dequeue());
-            }
             return (Numbers);
+        }
+        static int operatorPrecedence(string n)
+        {
+            int precedence = -1;
+            char c = n[0];
+            switch (c)
+            {
+                case '+':
+                case '-':
+                    precedence = 2;
+                    break;
+                case '*':
+                case '/':
+                    precedence = 3;
+                    break;
+                case '^':
+                    precedence = 4;
+                    break;
+
+                default:
+                    precedence = -1;
+                    break;
+            }
+            return precedence;
+        }
+        static double subtractValues(double a, double b)
+        {
+            double difference = b - a;
+            return difference;
+        }
+
+        static double addValues(double a, double b)
+        {
+            double sum = a + b;
+            return sum;
+        }
+
+        static double divideValues(double a, double b)
+        {
+            double quotient = b / a;
+            return quotient;
+        }
+
+        static double multiplyValues(double a, double b)
+        {
+            double product = a * b;
+            return product;
+        }
+
+        static double exponentValues(double a, double b)
+        {
+            double product = Math.Pow(b, a);
+            return product;
         }
         static void Main(string[] args)
         {
+            string[] arr = {"5" ,"+", "9"};
             Stack<string> Operators = new Stack<string>(args.Length);
             Queue<string> Numbers = new Queue<string>(args.Length);
-            ToPostFix(args, Operators, Numbers);
+            ToPostFix(arr, Operators, Numbers);
+            HashTable(Numbers);
         }
     }
 
